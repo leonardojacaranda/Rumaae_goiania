fd7875f11a08b5bca9b1434f6c036b9a0e0206c6
Tayrone Cordeiro de Menezes Marques <tayronecordeiro@hotmail.com>
2016-07-01 07:53:54 -0300

    commit tayrone

---
+++ app/src/main/java/com/example/leonardo/rumaae/Motivo.java
@@ -0,0 +1,14 @@
+package com.example.leonardo.rumaae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Motivo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_motivo);
+    }
+
+}
