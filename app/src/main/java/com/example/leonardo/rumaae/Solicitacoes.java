5a52cebdff0627984124079f44057f18e5d15693
Tayrone Cordeiro de Menezes Marques <tayronecordeiro@hotmail.com>
2016-07-14 15:41:18 -0300

    Revert "Revert "opçoes do menu lateral""
    This reverts commit 048918b5a647e79eb4e00b5ae70e1d9d6a774934.

--- app/src/main/java/com/example/leonardo/rumaae/Solicitacoes.java
+++ app/src/main/java/com/example/leonardo/rumaae/Solicitacoes.java
@@ -59,17 +59,13 @@ public class Solicitacoes extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_notificacoes) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_editar_perfil) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_trocar_senha) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sair) {
 
         }
 
